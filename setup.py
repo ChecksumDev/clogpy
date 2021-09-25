45a2f836dbbb2d6b6e06abb68415382591348f73
Checksum <collierdevs@tuta.io>
2021-09-25 16:21:20 -0500

    v1.0.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='clog',
+      version='1.0.0',
+      description='A simple python module for fast and efficient logging',
+      url='https://github.com/ChecksumDev/clogpy',
+      author='Checksum',
+      author_email='collierdevs@tuta.io',
+      license='GPLv3',
+      packages=['clog'],
+      package_dir={'clog': 'clog'},
+      zip_safe=False)
